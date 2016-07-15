e01773d26ac069f7b4600adffa0e15c2044e475b
Franz Holzinger <holzinger@ttproducts.de>
2016-07-15 21:14:43 +0200

    add missing backslash

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,4 +8,4 @@ define('EXPORT_TT_PRODUCTS_EXT', $_EXTKEY);
 
 
 // add the hook for the export extension
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['export']['hook'][$_EXTKEY] = 'JambageCom\\ExportTtProducts\\Hooks\ExportHooks';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['export']['hook'][$_EXTKEY] = 'JambageCom\\ExportTtProducts\\Hooks\\ExportHooks';
